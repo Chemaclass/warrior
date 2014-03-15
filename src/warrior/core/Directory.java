b9fa0b513c8706669135d8cccc9969166bede670
José María Valera Reales <chemaclass@outlook.es>
2014-03-15 14:58:29 +0100

    Añado dir.warrior.core

---
+++ src/warrior/core/Directory.java
@@ -0,0 +1,52 @@
+package warrior.core;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory {
+      
+    private File file;
+	private List<Directory> list;
+
+	public Directory(File file) {
+        this.file = file;
+        this.list = new ArrayList<Directory>();
+    }
+	
+	public Directory(File file, List<Directory> list) {
+        this.file = file;
+        this.list = list;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+	@SuppressWarnings("rawtypes")
+	public List getList() {
+        return list;
+    }
+
+	public void setList(List<Directory> list) {
+        this.list = list;
+    }
+
+	@Override
+	public String toString() {
+		String s = "path:\t"+file;
+		for(File f: file.listFiles())
+			s+="\n\t"+f;
+		if(list.size()>0)
+			for(Object o: list)
+				s+="\t\n"+o;
+		return s;
+	}
+    
+    
+    
+}
