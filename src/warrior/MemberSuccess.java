545c8a078483bee1dfa3b3b4482a305b348e8be9
José María Valera Reales <chemaclass@outlook.es>
2014-03-15 15:15:20 +0100

    Formateo dir.warrior

--- src/warrior/MemberSuccess.java
+++ src/warrior/MemberSuccess.java
@@ -5,52 +5,58 @@ import home.Str;
 import success.Core;
 import success.Ftp;
 
-
 public class MemberSuccess {
-	
-	private Ftp ftp;
-	private Core core;
-	
-	public MemberSuccess(){}
-	
-	public MemberSuccess(Ftp ftp, Core war) {
-		super();
-		this.ftp = ftp;
-		this.core = war;
-	}
 
-	public Ftp getFtp() {
-		return ftp;
-	}
+    private Ftp ftp;
+    private Core core;
 
-	public void setFtp(Ftp ftp) {
-		this.ftp = ftp;
-	}
+    public MemberSuccess() {
+    }
 
-	public Core getCore() {
-		return core;
-	}
+    public MemberSuccess(Ftp ftp, Core war) {
+        super();
+        this.ftp = ftp;
+        this.core = war;
+    }
 
-	public void setCore(Core log) {
-		this.core = log;
-	}
+    public Ftp getFtp() {
+        return ftp;
+    }
 
-	@Override
-	public String toString() {
-		String s = "";
-		if(ftp!=null)s+=ftp.getNotNull();
-		if(core!=null)s+=core.getNotNull();		
-		return (s.length()==0)?Str.empty:s;
-	} 
-	
-	public String getHelps(String key2) {
-		switch(key2){
-		case Str.ftp:return Ftp.getHelps();
-		case Str.core:return Core.getHelps();
-		case Str.all:return Core.getHelps()+"\n"+Ftp.getHelps()+"\n"+Help.getOthersHelps();
-		}
-		return "";
-	} 
-	
-	
+    public void setFtp(Ftp ftp) {
+        this.ftp = ftp;
+    }
+
+    public Core getCore() {
+        return core;
+    }
+
+    public void setCore(Core log) {
+        this.core = log;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if (ftp != null) {
+            s += ftp.getNotNull();
+        }
+        if (core != null) {
+            s += core.getNotNull();
+        }
+        return (s.length() == 0) ? Str.empty : s;
+    }
+
+    public String getHelps(String key2) {
+        switch (key2) {
+            case Str.ftp:
+                return Ftp.getHelps();
+            case Str.core:
+                return Core.getHelps();
+            case Str.all:
+                return Core.getHelps() + "\n" + Ftp.getHelps() + "\n" + Help.getOthersHelps();
+        }
+        return "";
+    }
+
 }
