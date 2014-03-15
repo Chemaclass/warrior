3ef8d48c72f8955db604c41fedd7997341a16ef6
José María Valera Reales <chemaclass@outlook.es>
2014-03-15 14:50:45 +0100

    Añado dir.member

---
+++ src/member/Member.java
@@ -0,0 +1,21 @@
+package member;
+
+import java.util.Map;
+
+@Deprecated
+public abstract class Member{
+
+	protected static Map<String,String> help, type;
+	
+	
+	public abstract String getHelps();
+	
+	public abstract String getTypes();
+	
+	public abstract String getNotNull();
+	
+	public abstract String getTypesNotNull();
+	
+	
+	
+}
