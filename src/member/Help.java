48938ef56702f1ff4a0fffc1d763781b1ce3255b
José María Valera Reales <chemaclass@outlook.es>
2014-03-15 15:14:24 +0100

    Formateo dir.member

--- src/member/Help.java
+++ src/member/Help.java
@@ -11,57 +11,70 @@ import success.Ftp;
 
 public class Help {
 
-	public final static String NAME = Str.help;
-	public final static String HELP = Str.help_to+NAME;
-	
-	public Help() {}
+    public final static String NAME = Str.help;
+    public final static String HELP = Str.help_to + NAME;
 
-	/**
-	 * 
-	 * @param key help
-	 * @param key2 
-	 * @param valueBuffer
-	 * @return
-	 */
-	public static String getHelp(String key2, String valueBuffer) {
-		
-		switch(key2){					
-		case Ftp.NAME:
-			switch(valueBuffer){
-				case Str.user:return Ftp.getHelp(Str.user);		
-				case Str.pass:return Ftp.getHelp(Str.pass);
-				case Str.server:return Ftp.getHelp(Str.server);
-			}
-			break;
-		case Core.NAME:
-			switch(valueBuffer){
-				case Str.all:return Core.getHelp(Str.all);
-				case Str.name:return Core.getHelp(Str.name);
-				case Str.path:return Core.getHelp(Str.path);
-				case Str.search:return Core.getHelp(Str.search);
-				case Str.send:return Core.getHelp(Str.send);
-				case Str.see:return Core.getHelp(Str.see);
-				case Str.write:return Core.getHelp(Str.write);
-			}
-			break;
-		}
-			
-		return null;
-	}
+    public Help() {
+    }
 
-	public static Map<String,String> help = new HashMap<String,String>();
-	static{
-		help.put(Str.clean,	 Str.h_clean);
-		help.put(Str.show,	 Str.h_show);
-		help.put(Str.help,	 Str.h_help);
-	}
-	
-	public static String getOthersHelps() {		
-			String s = "Other help:\n";
-			Iterator<Entry<String, String>> it = help.entrySet().iterator();
-			while(it.hasNext())
-				s += Str.out + it.next()+"\n";
-			return s;
-		}
-	
+    /**
+     *
+     * @param key help
+     * @param key2
+     * @param valueBuffer
+     * @return
+     */
+    public static String getHelp(String key2, String valueBuffer) {
+
+        switch (key2) {
+            case Ftp.NAME:
+                switch (valueBuffer) {
+                    case Str.user:
+                        return Ftp.getHelp(Str.user);
+                    case Str.pass:
+                        return Ftp.getHelp(Str.pass);
+                    case Str.server:
+                        return Ftp.getHelp(Str.server);
+                }
+                break;
+            case Core.NAME:
+                switch (valueBuffer) {
+                    case Str.all:
+                        return Core.getHelp(Str.all);
+                    case Str.name:
+                        return Core.getHelp(Str.name);
+                    case Str.path:
+                        return Core.getHelp(Str.path);
+                    case Str.search:
+                        return Core.getHelp(Str.search);
+                    case Str.send:
+                        return Core.getHelp(Str.send);
+                    case Str.see:
+                        return Core.getHelp(Str.see);
+                    case Str.write:
+                        return Core.getHelp(Str.write);
+                }
+                break;
+        }
+
+        return null;
+    }
+
+    public static Map<String, String> help = new HashMap<String, String>();
+
+    static {
+        help.put(Str.clean, Str.h_clean);
+        help.put(Str.show, Str.h_show);
+        help.put(Str.help, Str.h_help);
+    }
+
+    public static String getOthersHelps() {
+        String s = "Other help:\n";
+        Iterator<Entry<String, String>> it = help.entrySet().iterator();
+        while (it.hasNext()) {
+            s += Str.out + it.next() + "\n";
+        }
+        return s;
+    }
+
 }
--- src/member/Member.java
+++ src/member/Member.java
@@ -3,19 +3,16 @@ package member;
 import java.util.Map;
 
 @Deprecated
-public abstract class Member{
+public abstract class Member {
 
-	protected static Map<String,String> help, type;
-	
-	
-	public abstract String getHelps();
-	
-	public abstract String getTypes();
-	
-	public abstract String getNotNull();
-	
-	public abstract String getTypesNotNull();
-	
-	
-	
+    protected static Map<String, String> help, type;
+
+    public abstract String getHelps();
+
+    public abstract String getTypes();
+
+    public abstract String getNotNull();
+
+    public abstract String getTypesNotNull();
+
 }
